5632da99293f4fcb2d6ee11c4dbe1045429b8e4d
calves20 <157167670+calves20@users.noreply.github.com>
2024-04-10 22:59:26 -0700

    Update BridgeRunner.java

--- BridgeRunner.java
+++ BridgeRunner.java
@@ -1,20 +1,39 @@
-/**
- * Runs all threads
- */
-
+//Auhtor @Curtis Alves
 public class BridgeRunner {
+    public static void main(String[] args) {
+        // Check command line inputs
+        if (args.length != 2) {
+            System.out.println("Usage: java BridgeRunner <bridge limit> <num cars>");
+            return;
+        }
 
-	public static void main(String[] args) {
+        int bridgeLimit = Integer.parseInt(args[0]);
+        int numCars = Integer.parseInt(args[1]);
+        if (bridgeLimit <= 0 || numCars <= 0) {
+            System.out.println("Error: bridge limit and/or num cars must be positive.");
+            return;
+        }
 
-		// TODO - check command line inputs
+        // Instantiate the bridge
+        OneLaneBridge bridge = new OneLaneBridge(bridgeLimit);
 
-		// TODO - instantiate the bridge
-		
-		// TODO - allocate space for threads
+        // Allocate space for threads and start them
+        Thread[] carThreads = new Thread[numCars];
+        for (int i = 0; i < numCars; i++) {
+            Car car = new Car(i, bridge);
+            carThreads[i] = new Thread(car);
+            carThreads[i].start();
+        }
 
-		// TODO - start then join the threads
+        // Join the threads
+        for (int i = 0; i < numCars; i++) {
+            try {
+                carThreads[i].join();
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+        }
 
-		System.out.println("All cars have crossed!!");
-	}
-
+        System.out.println("All cars have crossed!!");
+    }
 }
